d9f159932f6a0e02998fb930fb425380facd85fa
nothing628 <yogyphang@gmail.com>
2026-06-03 20:30:45 +0700

    translate scss style to tailwindcss style

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import "./style.css";
-import "./styles/styles.scss";
+// import "./style.css";
+import "./styles/test.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "~/router";
