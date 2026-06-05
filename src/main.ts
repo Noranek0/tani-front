9d8fd828ef15459c41773fbe622173d9991ba64b
nothing628 <yogyphang@gmail.com>
2026-06-05 12:44:14 +0700

    add pages style

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import('./styles/base.css');
 import('./styles/bootstrap.css');
 import('./styles/custom.css');
 import('./styles/menu-styles.css');
+import('./styles/pages.css');
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "~/router";
